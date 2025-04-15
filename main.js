e044e7a49669fdb20644e3866346061fa4a64c58
HeitorDalla <heitorvillavilla@gmail.com>
2025-04-15 19:59:03 -0300

    Criação da logica para a mudança de tela e da estrutura inicial do cadastro.html

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 "use strict";
 
 // Configuração acesso para a tela de login
+
+function acessar () {
+    const loginEmail = document.querySelector("#loginEmail").value;
+    const loginSenha = document.querySelector("#loginSenha").value;
+
+    console.log(loginEmail)
+
+    // Implementação de estrutura que vai testar se todos os campos estão preenchidos
+    if (!loginEmail || !loginSenha) {
+        window.location.href = 'cadastro.html'; // Redirecionamento para o arquivo de cadastro.html
+    } else {
+        alert("Favor preencher todos os campos."); // Alerta de aviso ao usuario
+    }
+};
+
+const buttonEntrar = document.querySelector("#buttonEntrar");
+buttonEntrar.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    acessar();
+});
