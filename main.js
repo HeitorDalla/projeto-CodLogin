0cb8ab3cec472738df53dd366cb409ac9d1b0d41
HeitorDalla <heitorvillavilla@gmail.com>
2025-04-15 20:55:23 -0300

    Implementando funcionalidades de adicionar os nomes dinamicamente

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 "use strict";
 
-// Configuração acesso para a tela de login
+let dadosLista = []; // Array para armazenar as pessoas cadastradas
 
+// Função para o acesso a tela de cadastro
 function acessar () {
     const loginEmail = document.querySelector("#loginEmail").value;
     const loginSenha = document.querySelector("#loginSenha").value;
@@ -9,16 +10,41 @@ function acessar () {
     console.log(loginEmail)
 
     // Implementação de estrutura que vai testar se todos os campos estão preenchidos
-    if (!loginEmail || !loginSenha) {
+    if (loginEmail && loginSenha) {
         window.location.href = 'cadastro.html'; // Redirecionamento para o arquivo de cadastro.html
     } else {
         alert("Favor preencher todos os campos."); // Alerta de aviso ao usuario
     }
 };
 
-const buttonEntrar = document.querySelector("#buttonEntrar");
-buttonEntrar.addEventListener("click", (event) => {
+// Função para salvar os usuarios cadastrados
+function salvarUser () {
+    const nomeUser = document.querySelector("#nomeUser").value;
+
+    if (nomeUser) {
+        dadosLista.push(nomeUser);
+        nomeUser.value = ''; // Zera o texto do input
+    }
+};
+
+// Evento para acontecer quando o html for completamento carregado
+document.addEventListener("DOMContentLoaded", (event) => {
     event.preventDefault();
 
-    acessar();
+    // Funcionalidades no button de entrar
+    const buttonEntrar = document.querySelector("#buttonEntrar");
+    buttonEntrar.addEventListener("click", (event) => {
+        event.preventDefault();
+    
+        acessar();
+    });
+    
+    // Funcionalidades no button de salvar
+    const salvarUser = document.querySelector("#salvarUser");
+    salvarUser.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        salvarUser();
+    });
+
 });
